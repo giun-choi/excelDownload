c07f267df83af0e0ff7fe70755f499e5b80768ef
giun-choi <liamg8341@gmail.com>
2022-02-04 13:42:47 +0900

    addImage

--- src/components/TestTable.js
+++ src/components/TestTable.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ExcelJS from 'exceljs';
 import saveAs from 'file-saver';
+import download from '../imgs/download.png'
 
 const TestTable = () => {
 
@@ -11,6 +12,11 @@ const TestTable = () => {
 
         // mergeCells : A2 ~ I2 셀 병합
         worksheet.mergeCells("A2:I2");
+
+        // Workbook객체에 image를 추가하고 그 image의 id를 반환받는다.
+        const imageId1 = ExcelJSWorkbook.addImage({ base64: download, extension: 'png' });
+        // 반환 받은 이미지의 아이디를 B2 ~ D6 에 적용시 해당 영역까지 이미지 반영
+        worksheet.addImage(imageId1, 'B2:D6');
 
         ExcelJSWorkbook.xlsx.writeBuffer().then(function (buffer) {
             saveAs(
